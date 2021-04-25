2ec1b885c476fbd23d7e6d87b5f02800f7f5fa2f
matheuscarv69 <matheus9126@gmail.com>
2021-04-25 14:24:18 -0400

    :sparkles: feat: created connection by mongodb

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,18 @@ import "reflect-metadata";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
+
 import { ProductResolver } from "./resolvers/ProductResolver";
+import Database from "./database/Database";
 
 class App {
   public server: express.Application;
+  private database: Database;
 
   constructor() {
     this.server = express();
+    this.database = new Database();
+    this.database.createConnection();
 
     this.startApolloServer();
   }
---
+++ src/database/Database.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+class Database {
+  private DB_URL = 'mongodb://localhost:27017/promo-x';
+
+  createConnection() {
+    mongoose.connect(this.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+  }
+}
+
+export default Database;
